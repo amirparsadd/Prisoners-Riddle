74e8ba9dd757c82398edd998abe50e1d58e0e3b3
Amirparsa Baghdadi <amirparsab9@gmail.com>
2025-11-19 22:30:38 +0330

    optimize averaging

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 const shuffle = require("shuffle-array")
 
-const average = array => array.reduce((a, b) => a + b) / array.length
-
 const prisoners = 100 // Recommended = Powers Of 10
 const turns = Math.round(prisoners/2)
 const simulation_count = 15000 // Higher = More Accuracy
 
-let chance = []
+let chance = 0
 
 for (let i = 0; i < simulation_count; i++) {
+    let simulationChance = 0
+
     let boxes = []
     for (let i = 0; i < prisoners; i++) {
         boxes[i] = i
     }
 
     boxes = shuffle(boxes, { copy: true })
-    chance[i] = 0
 
     for (let j = 0; j < prisoners; j++) {
         lastval = j
@@ -23,7 +22,8 @@ for (let i = 0; i < simulation_count; i++) {
             lastval = boxes[lastval]
 
             if(lastval == j){
-                chance[i] += 1
+                simulationChance++
+                chance++
                 break
             }
         }
@@ -33,7 +33,7 @@ for (let i = 0; i < simulation_count; i++) {
         }
     }
 
-    console.log(`SIMULATION ${i} SURVIVAL: ${chance[i]}`)
+    console.log(`SIMULATION ${i} SURVIVAL: ${simulationChance}`)
 }
 
-console.log(average(chance)/prisoners)
+console.log(chance/simulation_count/prisoners)
